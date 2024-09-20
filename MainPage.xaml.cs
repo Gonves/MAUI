b709779d9b924d2d63521c0cd3810dbb239b2618
celio <celio@CELIOMARJUNIOR>
2024-09-20 20:38:27 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Kotlin.Properties;
+using System.Collections.ObjectModel;
+
+namespace MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+       public ObservableCollection<TaskItem> TaskList { get; set; } = new ObservableCollection<TaskItem> ();
+
+        public MainPage() 
+        { 
+            InitializeComponent ();
+            TaskListView.ItemsSource = TaskList;
+
+          
+        }
+
+        private void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TaskEntry.Text)) 
+            {
+                TaskList.Add(new TaskItem{ TaskName = TaskEntry.Text});
+                
+                TaskEntry.Text = string.Empty;
+
+            }
+
+        }
+
+        private void OnCompleteTaskClicked(object sender, EventArgs e) 
+        { 
+            var button =sender as Button;
+            var Task = button.BindingContext as TaskItem;
+            if (Task != null) 
+            {
+                Task.TaskName += "(Concluido)";
+                DisplayAlert("Tarefas Finalizadas", "A tarefa" + Task.TaskName, "Ok");
+            }
+        }
+    }
+
+}
