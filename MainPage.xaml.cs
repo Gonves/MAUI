27a3801a9694f888f83e981d4e8c8d764577fbc3
Celiomar Júnior <celiomarxd@gmail.com>
2024-09-20 21:19:41 -0300

    func

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,42 +1,52 @@
-﻿using Kotlin.Properties;
+﻿
 using System.Collections.ObjectModel;
 
 namespace MAUI
 {
     public partial class MainPage : ContentPage
     {
-       public ObservableCollection<TaskItem> TaskList { get; set; } = new ObservableCollection<TaskItem> ();
+        public ObservableCollection<TaskItem> TaskList{ get; set; } = new ObservableCollection<TaskItem>();
 
-        public MainPage() 
-        { 
-            InitializeComponent ();
+        public MainPage()
+        {
+            InitializeComponent();
             TaskListView.ItemsSource = TaskList;
 
-          
         }
 
         private void OnAddTaskClicked(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(TaskEntry.Text)) 
+            if (!string.IsNullOrEmpty(TaskEntry.Text))
             {
-                TaskList.Add(new TaskItem{ TaskName = TaskEntry.Text});
-                
+                TaskList.Add(new TaskItem { TaskName = TaskEntry.Text });
+
                 TaskEntry.Text = string.Empty;
 
             }
 
         }
 
-        private void OnCompleteTaskClicked(object sender, EventArgs e) 
-        { 
-            var button =sender as Button;
+        private void OnCompleteTaskClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
             var Task = button.BindingContext as TaskItem;
-            if (Task != null) 
+            if (Task != null)
             {
                 Task.TaskName += "(Concluido)";
                 DisplayAlert("Tarefas Finalizadas", "A tarefa" + Task.TaskName, "Ok");
             }
         }
-    }
 
+        private void OnDeleteTaskClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.TextColor = Colors.White;
+            var task = button.BindingContext as TaskItem;
+
+            if (task != null)
+            {
+                TaskList.Remove(task);
+            }
+        }
+    }
 }
---
+++ TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI
+{
+    public class TaskItem
+    {
+        public string TaskName {get; set;}
+    }
+}
